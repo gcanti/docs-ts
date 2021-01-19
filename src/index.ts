96b22654016dbc99952ee8a0baf4b35f600ffcfb
Maxwell Brown <maxwellbrown1990@gmail.com>
2021-01-18 23:03:51 -0500

    Achieve 100% test coverage (minus bin)

--- src/index.ts
+++ src/index.ts
@@ -9,30 +9,43 @@ import * as T from 'fp-ts/Task'
 import * as TE from 'fp-ts/TaskEither'
 
 import * as Core from './Core'
+import { Example } from './Example'
 import { FileSystem } from './FileSystem'
 import { Logger } from './Logger'
 
-const capabilities: Core.Capabilities = {
-  ...FileSystem,
-  ...Logger
-}
+// -------------------------------------------------------------------------------------
+// utils
+// -------------------------------------------------------------------------------------
 
-const exit = (code: 0 | 1): IO.IO<void> => () => process.exit(code)
+const exitProcess = (code: 0 | 1): IO.IO<void> => () => process.exit(code)
 
 const onLeft = (e: string): T.Task<void> =>
   T.fromIO(
     pipe(
       log(chalk.bold.red(e)),
-      IO.chain(() => exit(1))
+      IO.chain(() => exitProcess(1))
     )
   )
 
-const onRight: T.Task<void> = T.fromIO(log(chalk.bold.green('Docs generation succeeded!')))
+const onRight: T.Task<void> = pipe(
+  T.fromIO(log(chalk.bold.green('Docs generation succeeded!'))),
+  T.chain(() => T.fromIO(exitProcess(0)))
+)
 
 /**
+ * @category utils
  * @since 0.6.0
  */
-export const main: T.Task<void> = pipe(
-  Core.main(capabilities),
-  TE.fold(onLeft, () => onRight)
-)
+export const exit: (program: TE.TaskEither<string, void>) => T.Task<void> = TE.fold(onLeft, () => onRight)
+
+const capabilities: Core.Capabilities = {
+  example: Example,
+  fileSystem: FileSystem,
+  logger: Logger
+}
+
+/**
+ * @category utils
+ * @since 0.6.0
+ */
+export const main: T.Task<void> = pipe(Core.main(capabilities), exit)

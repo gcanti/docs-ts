a24c449e5b3e98102a9c1a1a84b7dab159219866
gcanti <giulio.canti@gmail.com>
2023-05-09 19:06:15 +0200

    remove lib folder

--- lib/Production.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * @since 0.8.0
- */
-import * as Core from './Core';
-/**
- * @category production
- * @since 0.8.0
- */
-export declare const capabilities: Core.Capabilities;
--- lib/bin.d.ts
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-/**
- * CLI
- *
- * @since 0.2.0
- */
-export {};
